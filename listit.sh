5a4c5432a116e03916f537d7d1fee5201a60e8ce
Ryan Pillay <35379549+rypillay@users.noreply.github.com>
2022-07-11 12:12:17 +0200

    Create listit.sh
    Added functionality for the following request:

---
+++ listit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# for use in bash/shell environments only
+# Make sure this script is executable (sudo chmod +x listit.sh)
+# Accepts and uses 1 input parameter (type integer) via $1 ARG. This is used to print X amount of lines in the file directories.list
+
+#create dir
+touch directories.list
+#lists all directories in current directory in line format, outputs stdout to directories.list file
+ls -dl */ > directories.list
+#inserts string into first line of directories.list
+sed -i "line one's line" directories.list
+#prints first $1 amount of lines in directories.list. $1 is the stdin first input parameter when executing the script 
+head -$1 directories.list | while read line; do echo $line; done
